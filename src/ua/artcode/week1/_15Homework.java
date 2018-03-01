ed27bfa8230240e427e2cab6f610a21cbd879442
Djipo <cosmo1@ukr.net>
2018-03-01 17:59:04 +0200

    fixed Homework 15

--- src/ua/artcode/week1/_15Homework.java
+++ src/ua/artcode/week1/_15Homework.java
@@ -1,43 +1,40 @@
 package ua.artcode.week1;
-//15) Известны площади круга и квадрата. Определить: а) уместится ли круг в
+//Известны площади круга и квадрата. Определить: а) уместится ли круг в
 //квадрат; б) уместится ли квадрат в круге.
 import java.util.Scanner;
+
 public class _15Homework {
 
-    public static void main (String [] args){
-        Scanner sc = new Scanner(System.in);
+          public static void main (String [] args){
+            Scanner sc = new Scanner(System.in);
 
-        System.out.println("Input Square Circle:");
-        double Square = sc.nextDouble();
+            System.out.println("Input Square Circle:");
+            double circleSqr = sc.nextDouble();
 
-        System.out.println("Input Square ");
-        double Square1 = sc.nextDouble();
+            System.out.println("Input Square ");
+            double squareSqr = sc.nextDouble();
 
-        double x1 = Square1 / 4;          // side of square
+            double radiusOfCircle = Math.sqrt(circleSqr / Math.PI);
+            double squareSide = Math.sqrt(squareSqr);
 
-        double Rv = x1 / 2;
-        double Ro = x1 / 1.4;
 
-        if (Rv > Square ){
-            System.out.println("Yes");
-        }else{
-            System.out.println("No");
+            double diametr = radiusOfCircle * 2;
+            // c^2 = a^2 + b^2
+            // c = sqrt(a^2 + b^2)
+            double diagonal = Math.sqrt(squareSide * squareSide + squareSide * squareSide);
+
+            if (diametr <= diagonal){
+                System.out.println("We can put the circle into the square");
+            }else{
+                System.out.println("We can not put circel into square");
+            }
+
+            if (diagonal <= diametr){
+                System.out.println("The Squere may be in the circle");
+            }else {
+                System.out.println("Impossible to put square into circle");
+            }
+
         }
-        if (Ro < Square){
-            System.out.println("Yes");
-        }else {
-            System.out.println("No");
-        }
-
-        }
-
-        }
-
-
-
-
-
-
-
-
+    }
 
